78d502fff14387314372bd8dbdae1462bc9ee59f
Reina.Maccredy <reina.maccredy@outlook.com>
2024-06-05 09:15:58 +0700

    Dia Update

--- src/TruuTuong/Dia.java
+++ src/TruuTuong/Dia.java
@@ -1,6 +1,19 @@
 package TruuTuong;
 import BinhThuong.ClassCha;
 public class Dia extends ClassCha {
-  
+    private int ThoiGian; // tinh bang phut
+    private String NXB; // Nha Xuat Ban
     
+    public Disc (String TuaDe, double GiaMua, int ThoiGian, String NXB, int ChatLuong, double GiaBan) {
+        super(TuaDe, GiaMua, ChatLuong, GiaBan);
+        this.ThoiGian = ThoiGian;
+        this.NXB = NXB;
+    }
+
+    @Override
+    public void inRa() {
+        super.inRa();
+        System.out.println("Thoi Gian: " + ThoiGian + " phut");
+        System.out.println("Nha Xuat Ban: " + NXB);
+    }
 }
